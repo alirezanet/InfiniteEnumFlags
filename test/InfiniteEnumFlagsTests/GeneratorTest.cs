dc2354455995ce945e0879bf60c9b0e4342882b8
AliReZa Sabouri <alirezanet@outlook.com>
2022-10-24 03:06:31 +0330

    test: add test project

---
+++ test/InfiniteEnumFlagsTests/GeneratorTest.cs
@@ -0,0 +1,19 @@
+using FluentAssertions;
+using Xunit;
+
+namespace InfiniteEnumFlagsTests;
+
+public class GeneratorTests
+{
+    [Fact]
+    public void name()
+    {
+        // Arrange
+
+        // Act
+
+        // Assert
+        true.Should().BeTrue();
+    }
+}
+
