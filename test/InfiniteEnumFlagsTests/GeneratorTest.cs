5a8f0412f6d1392b15ba501fb847d8df46d6c2bf
AliReZa Sabouri <alirezanet@outlook.com>
2022-10-24 14:54:50 +0330

    test: add arrange test classes

--- test/InfiniteEnumFlagsTests/GeneratorTest.cs
+++ test/InfiniteEnumFlagsTests/GeneratorTest.cs
@@ -10,7 +10,7 @@ public class GeneratorTests
     public void TestClass_MustHave_TotalItems_WithValueThree()
     {
         // Arrange
-        var x = new TestClass();
+        var x = new TestArrayFlags();
         var field = x.GetType().GetField("TOTAL_ITEMS");
 
         // Assert
@@ -19,12 +19,26 @@ public class GeneratorTests
     }
 }
 
-public partial class TestClass : IArrayFlags
+public partial class TestArrayFlags : IArrayFlags
 {
     public string[] Items() => new[]
     {
         "F1",
         "F2",
         "F3"
+    };
+}
+public partial class TestArrayFlagsWithoutItems : IArrayFlags
+{
+    public string[] Items() => throw new Exception("OOPS");
+}
+
+public partial class TestIndexDictionaryFlags : IIndexDictionaryFlags
+{
+    public Dictionary<string, int> Items() => new()
+    {
+        { "K2", 0 },
+        { "K3", 2 },
+        { "K1", 1 }
     };
 }
