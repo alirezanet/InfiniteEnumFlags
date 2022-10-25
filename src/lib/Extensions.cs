adfc0c8b04d34137f3c29ca6f62dce6bf179cd0b
AliReZa Sabouri <alirezanet@outlook.com>
2022-10-25 11:33:31 +0330

    feat: add HasFlag extension

---
+++ src/lib/Extensions.cs
@@ -0,0 +1,9 @@
+﻿namespace InfiniteEnumFlags;
+
+public static class Extensions
+{
+    public static bool HasFlag(this EnumItem source, EnumItem flag)
+    {
+        return (source & flag) != new EnumItem(0, source.Bits.Length);
+    }
+}
