8fb799f5d6e409a307dba6e784da5a8c0fa1cb48
AliReZa Sabouri <alirezanet@outlook.com>
2022-10-25 12:46:52 +0330

    test: add all extensions tests

--- test/InfiniteEnumFlagsTests/ExtensionsTest.cs
+++ test/InfiniteEnumFlagsTests/ExtensionsTest.cs
@@ -28,5 +28,49 @@ public class ExtensionsTest
         flags.HasFlag(Enums.TestArrayFlags.F3).Should().BeFalse();
         Enums.TestArrayFlags.None.HasFlag(Enums.TestArrayFlags.F3).Should().BeFalse();
     }
-}
 
+    [Fact]
+    public void ToggleFlag()
+    {
+        // Arrange
+        var flags = Enums.TestArrayFlags.F1 | Enums.TestArrayFlags.F2;
+
+        // Act
+        var actual = flags.ToggleFlag(Enums.TestArrayFlags.F1, Enums.TestArrayFlags.F3);
+
+        // Assert
+        actual.Should().Be(Enums.TestArrayFlags.F2 | Enums.TestArrayFlags.F3);
+        actual.Should().NotBe(Enums.TestArrayFlags.F1);
+        actual.Should().NotBe(Enums.TestArrayFlags.None);
+        actual.Should().NotBe(Enums.TestArrayFlags.All);
+    }
+
+    [Fact]
+    public void SetFlag()
+    {
+        // Arrange
+        var flags = Enums.TestArrayFlags.F1;
+
+        // Act
+        var actual = flags.SetFlag(Enums.TestArrayFlags.F3);
+
+        // Assert
+        actual.HasFlag(Enums.TestArrayFlags.F3);
+    }
+
+    [Fact]
+    public void UnSetFlag()
+    {
+        // Arrange
+        var flags = Enums.TestArrayFlags.F1 | Enums.TestArrayFlags.F2;
+
+        // Act
+        var actual = flags.UnsetFlag(Enums.TestArrayFlags.F1, Enums.TestArrayFlags.F3);
+
+        // Assert
+        actual.Should().Be(Enums.TestArrayFlags.F2);
+        actual.Should().NotBe(Enums.TestArrayFlags.F1);
+        actual.Should().NotBe(Enums.TestArrayFlags.None);
+        actual.Should().NotBe(Enums.TestArrayFlags.All);
+    }
+}
