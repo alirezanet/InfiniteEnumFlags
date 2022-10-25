4e981235d21f19f37af8e34ba08a2db734bc997d
AliReZa Sabouri <alirezanet@outlook.com>
2022-10-25 11:33:54 +0330

    test: add hasFlag test

---
+++ test/InfiniteEnumFlagsTests/ExtensionsTest.cs
@@ -0,0 +1,32 @@
+﻿using FluentAssertions;
+using InfiniteEnumFlags;
+using Xunit;
+
+namespace InfiniteEnumFlagsTests;
+
+public class ExtensionsTest
+{
+    [Fact]
+    public void HasFlag_TrueCondition()
+    {
+        // Arrange
+        var flags = Enums.TestArrayFlags.F1 | Enums.TestArrayFlags.F2;
+
+        // Assert
+        flags.HasFlag(Enums.TestArrayFlags.F2).Should().BeTrue();
+        flags.HasFlag(Enums.TestArrayFlags.F1).Should().BeTrue();
+        Enums.TestArrayFlags.All.HasFlag(Enums.TestArrayFlags.F3).Should().BeTrue();
+    }
+
+    [Fact]
+    public void HasFlag_FalseCondition()
+    {
+        // Arrange
+        var flags = Enums.TestArrayFlags.F1 | Enums.TestArrayFlags.F2;
+
+        // Assert
+        flags.HasFlag(Enums.TestArrayFlags.F3).Should().BeFalse();
+        Enums.TestArrayFlags.None.HasFlag(Enums.TestArrayFlags.F3).Should().BeFalse();
+    }
+}
+
