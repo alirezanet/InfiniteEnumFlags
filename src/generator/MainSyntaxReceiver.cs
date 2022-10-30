241ffc96627be9568dfb19eaea48f9288d035f2f
AliReZa Sabouri <alirezanet@outlook.com>
2022-10-30 16:51:24 +0330

    fix: remove EnumSyntaxReceiver

--- src/generator/MainSyntaxReceiver.cs
+++ src/generator/MainSyntaxReceiver.cs
@@ -4,11 +4,9 @@ namespace InfiniteEnumFlags.Generator;
 internal class MainSyntaxReceiver : ISyntaxReceiver
 {
     public ClassSyntaxReceiver ClassSyntax { get; } = new();
-    public EnumSyntaxReceiver EnumSyntax { get; } = new();
 
     public void OnVisitSyntaxNode(SyntaxNode syntaxNode)
     {
         ClassSyntax.OnVisitSyntaxNode(syntaxNode);
-        EnumSyntax.OnVisitSyntaxNode(syntaxNode);
     }
 }
