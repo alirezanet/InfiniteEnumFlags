ad2fdd2034ee3ca5a60d519bf20feafbd0f6384b
AliReZa Sabouri <alirezanet@outlook.com>
2022-11-08 14:02:55 +0330

    test: remove unused classes

--- test/InfiniteEnumFlagsTests/Enums/TestArrayFlagsWithoutItems.cs
+++
@@ -1,8 +0,0 @@
-﻿using InfiniteEnumFlags;
-
-namespace InfiniteEnumFlagsTests.Enums;
-
-public partial class TestArrayFlagsWithoutItems : IArrayFlags
-{
-    public string[] Items() => throw new Exception("OOPS");
-}
